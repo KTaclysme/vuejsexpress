fff7d4b1cb736be569a5493a965261e7bc9dd662
KTaclysme <takevin.pro@gmail.com>
2023-12-10 18:38:34 +0100

    movieApi

--- public/vue-project/src/views/TuttiFrutti.vue
+++ public/vue-project/src/views/TuttiFrutti.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
+const apiKey = "11920b0";
 
+//Description du film
+fetch(`http://www.omdbapi.com/?apikey=${apiKey}&`)
+    .then(response => response.json())
+    .then(data => {
+        // Handle the data here
+        console.log(data);
+    })
+    .catch(error => {
+        // Handle errors here
+        console.error('Error:', error);
+    });
+
+//Poster du film
+fetch(`http://img.omdbapi.com/?apikey=${apiKey}&`)
+    .then(response => response.json())
+    .then(data => {
+        // Handle the data here
+        console.log(data);
+    })
+    .catch(error => {
+        // Handle errors here
+        console.error('Error:', error);
+    });
 </script>
 
 <template>
@@ -7,5 +31,8 @@
         <h1>
             BIENVENU SUR TUTTI FRUTTI
         </h1>
+        <input @change="">
     </main>
 </template>
+
+<style scoped></style>
