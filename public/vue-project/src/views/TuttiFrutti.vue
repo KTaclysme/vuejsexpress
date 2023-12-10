353c380872b1af410b2c3ead1bece95e40e6ac81
KTaclysme <takevin.pro@gmail.com>
2023-12-10 21:31:47 +0100

    movieApi

--- public/vue-project/src/views/TuttiFrutti.vue
+++ public/vue-project/src/views/TuttiFrutti.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 const apiKey = "11920b0";
 
-// http://www.omdbapi.com/?s=interstellar&page=1&apiKey=11920b0
-// http://www.omdbapi.com/?i=tt0816692&apiKey=11920b0
+// http://www.omdbapi.com/?s=interstellar&page=1&apiKey=11920b0 avoir la page avoir l'id
+// http://www.omdbapi.com/?i=tt0816692&apiKey=11920b0 avoir les info via l'id imbd
+// http://img.omdbapi.com/?apikey=[yourkey]& avoir le poster
+// avoir l'id -> avoir l'info et le poster avec l'id 
+
 
 fetch(`http://www.omdbapi.com/?apikey=${apiKey}`)
     .then(response => response.json())
